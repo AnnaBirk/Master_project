9099bdb5f56adac5df633902ea7e5f76939ec232
Anna Birk Hellenes <annabirkhellenes@gmail.com>
2019-11-15 16:27:19 +0100

    started cleaning/commenting three-terminal_self...

--- Source_code/Three-terminal_selfenergy_method/figures/thesis_plots_Anna/Three-terminal_selfenergy_method_plots.py
+++ Source_code/Three-terminal_selfenergy_method/figures/thesis_plots_Anna/Three-terminal_selfenergy_method_plots.py
@@ -1,102 +1,140 @@
 """
-Plotting already generated data for self-energy model of three-terminal device,
+In this file, we plot already generated data for self-energy model of three-terminal device,
 using functions from the module thesis_plot_library.
 """
-
-from thesis_plot_library.thesis_plot_library import *
+from thesis_plot_library.thesis_plot_library import \
+                                plot_pcolormesh_4subplots_wzoom_a, \
+                                calc_plot_wf2_vs_var_and_site, \
+                                calc_dEdvar, \
+                                calc_QLR, \
+                                calc_E1_Em1_from_E0_E0prime_var, \
+                                plot_E0_gS_gA_and_QR_dE0_gSovergA_4subplots, \
+                                calc_Q_dE_mu_b_phase_shift, calc_wf_data, plot_Pl_and_cut_wf_spinpol_Q
+import numpy as np
 import sys
 from pathlib import Path
 sys.path.append(str(Path('.').absolute().parent))
 sys.path.append('../../')
 
-########## Plot Asymmetric differential conductances: ##########
-var = "mu"
-var_2 = "b"
-filepath = "wire_101/sites=800/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180"
 
-filename_a = "diff_cond_12_of_mu_in_-600-600_t=47118.61755164704_B=400 (copy)"
-filepath_a = (filepath + "/diff_cond_of_" + var)
-data_a = np.load(
-            "data/" + filepath_a + "/data_" + filename_a +  ".npz"
+""" 
+                PART 1 : ANTISYMMETRIC DIFFERENTIAL CONDUCTANCE
+
+Load antisymmetric differential conductances: """
+var_mu = "mu"
+var_b = "b"
+# filepath skeleton, path specified by folders whose names are specified by
+# the system parameters : 
+filepath_skeleton = "wire_101/sites=800/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180"
+
+# filename of differential conductance for chemical potential in 
+# the domain from -600 to 600 micro electron volts : 
+filename_mu = "diff_cond_12_of_mu_in_-600-600_t=47118.61755164704_B=400 (copy)"
+filepath_mu = (filepath_skeleton + "/diff_cond_of_" + var_mu)
+# loading differential conductance for chemical potential in said domain :
+data_mu = np.load(
+            "data/" + filepath_mu + "/data_" + filename_mu +  ".npz"
             )
-
-filename_ = "diff_cond_12_of_mu_in_-150-150_t=47118.61755164704_B=400 (copy)"
-filepath_ = (filepath + "/diff_cond_of_" + var)
-data_a_ = np.load(
-            "data/" + filepath_ + "/data_" + filename_ +  ".npz"
+# loading dfferential conductance for chemical potential in shorter domain,
+# inside the topological region. The charge can be extracted from the oscillations
+# in this domain :
+filename_mu_zoom = "diff_cond_12_of_mu_in_-150-150_t=47118.61755164704_B=400 (copy)"
+filepath_mu_zoom = (filepath_skeleton + "/diff_cond_of_" + var_mu)
+data_mu_zoom = np.load(
+            "data/" + filepath_mu_zoom + "/data_" + filename_mu_zoom +  ".npz"
             )
-
+# loading differential conductance for magnetic field in the domain
+# 0 to 800 micro electron volts ... :
 filename_b = "diff_cond_12_of_b_in_0-800_t=47118.61755164704 (copy)"
-filepath_b = (filepath + "/diff_cond_of_" + var_2)
-data_a_b = np.load(
+filepath_b = (filepath_skeleton + "/diff_cond_of_" + var_b)
+data_b = np.load(
             "data/" + filepath_b + "/data_" + filename_b +  ".npz"
             )
-
+# ... and in the smaller domain, from 200 to 800 micro electron volts,
+# where oscillations in the differential conductance are clear, and where
+# the charge can be probed :
 filename_b_zoom = "diff_cond_12_of_b_in_200-800_t=47118.61755164704 (copy)"
-filepath_b_zoom = (filepath + "/diff_cond_of_" + var_2)
-data_a_b_zoom = np.load(
+filepath_b_zoom = (filepath_skeleton + "/diff_cond_of_" + var_b)
+data_b_zoom = np.load(
             "data/" + filepath_b_zoom + "/data_" + filename_b_zoom +  ".npz"
             )
+# the data loaded above is in an .nzip folder containing three different
+# arrays. These arrays can be accessed as you would a dictionary. The
+# arrays have the following names :
+#       "x_range" :     np.array, containing all values of the independent
+#                       variable, here the chemical potential, or the magnetic
+#                       field.
+#       "e_range" :     np.array, containing all values of the energies the
+#                       differential conductance is calculated for.
+#       "diff_cond" :   np.array, containing all differential conductance
+#                       data points, calculated in "x_range"-"e_range" "space".
+#                       It has the shape
+#                               ( np.shape(e_range), np.shape(x_range) )
+#                       in the following :
+mu_range, e_range_mu, diff_cond_mu = \
+        data_mu["x_range"], data_mu["e_range"], data_mu["diff_cond"]
+mu_range_zoom, e_range_mu_zoom, diff_cond_mu_zoom = \
+        data_mu_zoom["x_range"], data_mu_zoom["e_range"], data_mu_zoom["diff_cond"]
+b_range, e_range_b, diff_cond_b = \
+        data_b["x_range"], data_b["e_range"], data_b["diff_cond"]
+b_range_zoom, e_range_b_zoom, diff_cond_b_zoom = \
+        data_b_zoom["x_range"], data_b_zoom["e_range"], data_b_zoom["diff_cond"]
 
-x_range, e_range, diff_cond_a = \
-        data_a["x_range"], data_a["e_range"], data_a["diff_cond"]
-x_range_zoom, e_range_zoom, diff_cond_a_zoom = \
-        data_a_["x_range"], data_a_["e_range"], data_a_["diff_cond"]
-x_range_2, e_range_2, diff_cond_a_2 = \
-        data_a_b["x_range"], data_a_b["e_range"], data_a_b["diff_cond"]
-x_range_2_zoom, e_range_2_zoom, diff_cond_a_2_zoom = \
-        data_a_b_zoom["x_range"], data_a_b_zoom["e_range"], data_a_b_zoom["diff_cond"]
 
-#################### Plot Asymmetric differential conductances:
+""" Plot Asymmetric differential conductances : """
 fig, ax, cbar = plot_pcolormesh_4subplots_wzoom_a(\
-                        # 1st variable w zoom:
-                        x_range, e_range, diff_cond_a, 
-                        x_range_zoom, e_range_zoom, diff_cond_a_zoom,\
-                        # 2nd variable w zoom:
-                        x_range_2, e_range_2, diff_cond_a_2,\
-                        x_range_2_zoom, e_range_2_zoom, diff_cond_a_2_zoom,\
-                        # specifications:
-                        colormap_log_2_value =0.003,
-                        colormap_limits=[False,-0.011,0.011],\
-                        colormap_norm=[True, 0], \
-                        colormap_name='seismic',#'RdBu_r',
-                        colormap_log=[True, 0.0001])
+        # This function plots differential conductance in four panels :
+        #  - upper left and right panels : 
+        #       zoomed "out" differential conductance, in mu versus energy (left)
+        #       and b versus energy (right) space
+        #  - lower left and right panels :
+        #       zoomed "in" differential conductance, --''--.
+                        # 1st variable (left upper and lower panels):
+                        mu_range, e_range_mu, diff_cond_mu, 
+                        mu_range_zoom, e_range_mu_zoom, diff_cond_mu_zoom,\
+                        # 2nd variable (right upper and lower panels):
+                        b_range, e_range_b, diff_cond_b,\
+                        b_range_zoom, e_range_b_zoom, diff_cond_b_zoom,\
+                        # plotting specifications:
+                        colormap_log_2_value =0.003,    # lower cut-off value for log-scale
+                                                        # (see matplotlib documentation)
+                        colormap_limits=[False,-0.011,0.011],   # If first element is False, use automatic limits set
+                                                                # in matplotlib. If first element is True, use the
+                                                                # lower and upper limit, specified by the second and
+                                                                # third elements, respectively.
+                        colormap_norm=[True, 0],        # Sets the midpoint in the colormap to be at the value
+                                                        # zero. With a scale with different colors for values above
+                                                        # and below zero, one can easily see the difference between
+                                                        # positive and negative differential conductance in the plot.
+                        colormap_name='seismic',        # colormapping between values of data
+                                                        # and colors plotted.
+                        colormap_log=[True, 0.0001])    # If first element is True, the colormap is scaled
+                                                        # logarithmically. The specific scaling is then 
+                                                        # specified by the second element. If the first element
+                                                        # is false, the map is scaled linearly.
 
 ft = "pdf"
-fig.savefig("figures/" + filepath + "/thesis_plots_Anna" + "/fig_" \
-                                + filename_a + "." + ft, format=ft, dpi=300)
+fig.savefig("figures/" + filepath_skeleton + "/thesis_plots_Anna" + "/fig_" \
+                                + filename_mu + "." + ft, format=ft, dpi=300)
 fig.show()
-#################################################################
 
 
 
-###########     PLOTTING WAVEFUNCTION/SPIN POLARIZATION/CHARGE 
-#               as function of lattice sites in real space      ###########
-var = "mu"
-var_2 = "b"
-filepath = "wire_101/sites=800/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180"
-filepath = "wire_101/sites=100/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180"
-filepath = "wire_101/sites=200/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180"
+"""     
+        PART 2 : DIFFERENTIAL CONDUCTANCE SPECTROSCOPY
 
-filepath_b = (filepath + "/Psi_components_vs_b")
-filename_b = "Psi_=b_in_200-800 (another copy)"
+Loading data :
+"""   
+filepath_skeleton = "wire_101/sites=200/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180"
+
+filepath_b = (filepath_skeleton + "/Psi_components_vs_b")
 filename_b = "Psi_=b_in_0-800"
 data_b =np.load(
                 "data/" + filepath_b + "/data_" + filename_b + ".npz"
                 )
 
-filepath_mu = (filepath + "/Psi_components_vs_mu")
-filepath_mu = ("../Jeroen tight binding code/data/")
-filepath_mu = "wire_101/sites=100/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180" \
-                                        + "/Psi_components_vs_mu/"
-filepath_mu = "wire_101/sites=200/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180" \
-                                        + "/Psi_components_vs_mu/"
-
-
-filename_mu = "Psi_=mu_in_-150-150_B=400 (another copy)" # old domain, far inside top region
-filename_mu = "dat_190529_114228" # new larger trial domain with Jeroen model
+filepath_mu = (filepath_skeleton + "/Psi_components_vs_mu/")
 filename_mu = "Psi_=mu_in_-600-600_B=400" # new trial domain larger
-
 data_mu = np.load(
                  "data/" + filepath_mu + "/data_" + filename_mu + ".npz"
                  )
@@ -109,13 +147,12 @@ b = data_b["var_range"]
 wf_b = data_b["psi_components"]
 E0_b = data_b["energies"][:,0]
 
-# Regions of well-behaved energy 
+# Regions of well-behaved energy, found by inspection and hard-coded
 # (level-crossings, with not-near zero energy modes, have not occured inside said regions) : 
 b_domain = 500
 b_domain_2 = 975
 mu_domain_2 = 792 
 mu_domain = 317
-
 
 """ 
 'Cutting and pasting' lowest-energy modes 
@@ -171,11 +208,12 @@ calculated from an infinite Oreg-Lutchyn wire with the same parameters as in thi
 Dtop_mu_data = np.load("Anna thesis plots/Dtop_mu_1.npz")
 Dtop_b_data = np.load("Anna thesis plots/Dtop_b_1.npz")
 
-
 """
-Plotting the probability density, spin polarization, and the charge, as functions of 
-        lambda = chemical potential, or magnetic field, on the x-axis, and
-        sites (real space) on the y-axis. 
+                PROBABILITY DENSITY/SPIN POLARIZATION/BCS CHARGE IN REAL SPACE
+Plotting the 
+        probability density, spin polarization, and the charge, as functions of 
+                lambda = chemical potential, or magnetic field, on the x-axis, and
+                sites (real space) on the y-axis. 
 """
 fig, ax, fig_spin, fig_Q, wf2_P_l_A_L_mu_b, spin_P_l_A_L_mu_b, Q_P_l_A_L_mu_b \
                                 = calc_plot_wf2_vs_var_and_site(   
@@ -188,37 +226,35 @@ fig, ax, fig_spin, fig_Q, wf2_P_l_A_L_mu_b, spin_P_l_A_L_mu_b, Q_P_l_A_L_mu_b \
                                                     (QR_b_i+(dEdb_i))/2., ph_b_i
                                                 ],
                                     Q_realspace=[	True,
-													dEdmu,
-													dEdb
-												],
+                                                        dEdmu,
+                                                        dEdb
+                                                ],
                                     Dtop2 = [		True,  
-													Dtop_mu_data["var_range"], 
-													Dtop_mu_data["D_top"] ,\
-                                                    Dtop_b_data["var_range"], 
-													Dtop_b_data["D_top"]
-											],
+                                                        Dtop_mu_data["var_range"], 
+                                                        Dtop_mu_data["D_top"] ,\
+                                                        Dtop_b_data["var_range"], 
+                                                        Dtop_b_data["D_top"]
+                                                ],
                                     level_cross_mu = [True,409,'gray'] )
 
 
-########        PLOTTING TRACES 
-#               of conductances along the two lowest energies   ########
-var = "mu"
-var_2 = "b"
-filepath = "wire_101/sites=800/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180"
+"""             DIFFERENTIAL CONDUCTANCE SPECTROSCOPY
+                        along the two lowest eigenenergies """
+                        
+filepath_skeleton = "wire_101/sites=800/length=1.5/alpha=0.280204287582202eVA/Delta_ind=180"
 
 # plot gs, ga (traced), E0; QR, dE, gS/gA:
-filepath_b = (filepath + "/Psi_components_vs_b")
+filepath_b = (filepath_skeleton + "/Psi_components_vs_b")
 filename_b = "Psi_=b_in_200-800_gS_and_gA_line_E0_Q__tunnel_t=47118.61755164704 (another copy)"
 data_b =np.load(
                 "data/" + filepath_b + "/data_" + filename_b + ".npz"
                 )
 
-filepath_mu = (filepath + "/Psi_components_vs_mu")
+filepath_mu = (filepath_skeleton + "/Psi_components_vs_mu")
 filename_mu = "Psi_=mu_in_-150-150_B=400_gS_and_gA_line_E0_Q__tunnel_t=47118.61755164704 (another copy)"
 data_mu = np.load(
                  "data/" + filepath_mu + "/data_" + filename_mu + ".npz"
                  )
-
 fig,ax, data_phaseshifts = plot_E0_gS_gA_and_QR_dE0_gSovergA_4subplots(   
                                             mu = data_mu["x_range"],
                                             E0_mu = data_mu["E0"],
@@ -257,11 +293,11 @@ ax[0,0].annotate(r'$\triangle\hspace{5pt}$',
             verticalalignment='bottom'
             )
 
-filename_a = "E0_gS_gA_and_QR_dE0_gSovergA_4subplots"
-print(" - Filepath where conductance trace plot is saved : ", filepath)
+filename_mu = "E0_gS_gA_and_QR_dE0_gSovergA_4subplots"
+print(" - Filepath where conductance trace plot is saved : ", filepath_skeleton)
 ft = "pdf"
-fig.savefig("figures/" + filepath + "/thesis_plots_Anna" + "/fig_" \
-                                + filename_a + "." + ft, format=ft, dpi=300)
+fig.savefig("figures/" + filepath_skeleton + "/thesis_plots_Anna" + "/fig_" \
+                                + filename_mu + "." + ft, format=ft, dpi=300)
 fig.show()
 
 
@@ -290,7 +326,7 @@ fig.show()
 
 
 """ The shifts (on the x-axis) of integrated versus end charges, saved as a numpy zip file : """
-np.savez("figures/" + filepath + "/thesis_plots_Anna" + "/data_phaseshifts" + ".npz", 
+np.savez("figures/" + filepath_skeleton + "/thesis_plots_Anna" + "/data_phaseshifts" + ".npz", 
                 phase_shifts_mu=data_phaseshifts[0], 
                 phase_shifts_b=data_phaseshifts[1], 
                 QR_mu_zeros_and_extrema=data_phaseshifts[2], 
